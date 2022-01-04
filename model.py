55426c05212f9930616de10e6f09a256507aafb7
Amy-Liao <72532191+Amy-Liao@users.noreply.github.com>
2022-01-04 21:48:05 +0800

    Update model.py

--- model.py
+++ model.py
@@ -5,28 +5,22 @@ from sklearn.linear_model import LinearRegression
 def preprocessing(country):
     rows = data.loc[(data['countriesAndTerritories'] == country)]
     x = rows['dateRep'].iloc[::-1].reset_index(drop=True)
-
     for i in range(x.size):
         x[i] = i
     size = x.size
-
     secondx=[]
     for j in range (size):
         secondx.insert(j, [j])
-
     y = rows['cases'].iloc[::-1].reset_index(drop=True)
     secondy=[]
     for s in range (size):
         secondy.insert(s, y[s])
-
     return secondx,secondy,size
 
 '''train the model'''
 def train(x,y,size):
-
     x_train = x[int(size*0.3):]
     y_train = y[int(size*0.3):]
-
     #build model
     model = LinearRegression()
     model.fit(x_train, y_train)
@@ -44,7 +38,6 @@ def main():
         x,y, size = preprocessing(countrylist[k])
         predictions = train(x,y, size)
         l.append(predictions)
-
     dat = ['10/9', '10/10', '10/11', '10/12', '10/13', '10/14', '10/15']
     result = pd.DataFrame(zip(*l), columns = countrylist, index=dat)
     result.to_csv('StudentID_HW1.csv')
